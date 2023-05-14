fc0a4341a1f07b6d85de846b06639deef94f1dba
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-05-14 15:00:54 -0400

    reporters  seeded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Reporter;
 use App\Models\Type;
 use App\Models\Project;
 use Illuminate\Database\Seeder;
@@ -18,10 +19,12 @@ class DatabaseSeeder extends Seeder
     {
 
         User::truncate();
+        Reporter::truncate();
         Type::truncate();
         Project::truncate();
         
         User::factory()->count(2)->create();
+        Reporter::factory()->count(2)->create();
         Type::factory()->count(3)->create();
         Project::factory()->count(4)->create();
             
