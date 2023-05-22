7b0a3f9c75fad34951ec7073c711a8af2dff7cfd
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-05-22 11:06:27 -0400

    store method for form

--- app/Http/Controllers/SegmentFormController.php
+++ app/Http/Controllers/SegmentFormController.php
@@ -17,12 +17,37 @@ class SegmentFormController extends Controller
         return view('segment_forms.list', compact('segments'));
     }
 
-  public function addForm(Request $request)
-{
-    $segment_type_id = $request->query('segment_type_id');
-    $segmentFields = SegmentField::all();
-    return view('segment_forms.add', compact('segment_type_id', 'segmentFields'));
+    public function addForm(Request $request)
+    {
+        $segment_type_id = $request->query('segment_type_id');
+        $segmentFields = SegmentField::all();
+        return view('segment_forms.add', compact('segment_type_id', 'segmentFields'));
+    }
+
+    public function store()
+    {
+        // Validate the form data
+        $attributes = request()->validate([
+            'title' => 'required',
+            // Add validation rules for other fields
+        ]);
+
+        // Create a new segment instance
+        $segment = new Segment();
+
+        // Assign the form data to the segment model
+        $segment->title = request('title');
+        // Assign other form fields to the corresponding model attributes
+
+        // Save the segment
+        $segment->save();
+
+        // Optionally, you can redirect to a success page or perform additional actions
+
+        // Redirect to the segment list page
+        return redirect()->route('segment_forms.list');
+    }
 }
 
     
-}
+
